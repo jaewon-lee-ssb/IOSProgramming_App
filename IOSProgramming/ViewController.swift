5a1fd5c2f05ab1cac72de2740d695a909721f602
LeeJaeWon <jaewon990905@gmail.com>
2021-05-23 04:49:38 +0900

    add gitignore

--- IOSProgramming/ViewController.swift
+++ IOSProgramming/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //test
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
