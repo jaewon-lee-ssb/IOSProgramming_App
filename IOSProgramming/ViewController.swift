34cd424a1bce3ab12c7c0421bf5f90b1ac2a2c76
LeeJaeWon <jaewon990905@gmail.com>
2021-05-23 15:56:42 +0900

    UI Setting

--- IOSProgramming/ViewController.swift
+++ IOSProgramming/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    //test
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerDataSource = ["광진구", "구로구", "동대문구", "종로구"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.pickerView.delegate = self;
+        self.pickerView.dataSource = self;
     }
 
 
